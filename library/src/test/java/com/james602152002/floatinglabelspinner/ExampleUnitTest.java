30ea216625a669e587765f92075035b5e31a68e0
= <=>
2018-03-16 16:14:47 +0800

    update test

--- library/src/test/java/com/james602152002/floatinglabelspinner/ExampleUnitTest.java
+++ library/src/test/java/com/james602152002/floatinglabelspinner/ExampleUnitTest.java
@@ -1,8 +1,7 @@
 package com.james602152002.floatinglabelspinner;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
